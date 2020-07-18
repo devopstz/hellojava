9ae6819bd5443c218bb2db25eab820cd34314d88
devopstz <68483271+devopstz@users.noreply.github.com>
2020-07-19 00:51:33 +0300

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,6 @@
-class Simple{  
-    public static void main(String args[]){  
-     System.out.println("Hello Java");  
-    }  
+public class Hello {
+  public static void main(String[] args){
+    //print hello world in the terminal
+	System.out.println("Hello, Jenkins and Java");
+  }
 }
